a12f48466befbed90145bf6b4d5de02ac7682cbc
ByFaithco01 <cnpbymf2jd@privaterelay.appleid.com>
2025-11-21 08:37:32 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,23 @@
-export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDd5Roefu920D3lp2Wd1ogFBANhoCPkk28",
+    authDomain: "byfaithco-c135c.firebaseapp.com",
+    projectId: "byfaithco-c135c",
+    storageBucket: "byfaithco-c135c.firebasestorage.app",
+    messagingSenderId: "51082695190",
+    appId: "1:51082695190:web:8b0574965fced9c4859b37",
+    measurementId: "G-E00608SK2Z"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
